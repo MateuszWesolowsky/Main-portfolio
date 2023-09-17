1bc9bf20af7bd31c81571962cbc89fd2a86faa7f
Mateusz <m.wesolowski.92@gmail.com>
2023-09-17 21:05:56 +0200

    create message comp for coppy email

---
+++ src/components/TopMessage.tsx
@@ -0,0 +1,37 @@
+import { ReactNode, useEffect, useState } from "react";
+
+type Props = {
+	duration?: number;
+	transition?: number;
+	onHide: () => void;
+	children: ReactNode;
+};
+
+const TopMessage = ({
+	duration = 1000,
+	transition = 0.5,
+	onHide,
+	children,
+}: Props) => {
+	const [hidden, sethidden] = useState(false);
+
+	useEffect(() => {
+		const transitionDelay = setTimeout(() => {
+			sethidden(true);
+		}, duration);
+		const hide = setTimeout(() => {
+			onHide();
+		}, duration + transition * 100);
+		return () => {
+			clearTimeout(hide);
+			clearTimeout(transitionDelay);
+		};
+	}, [duration, hidden, transition, onHide]);
+	return (
+		<div className='text-textPrimary top-10 bg-hoverSecondary rounded-full fixed p-2.5 xl:left-1/2 xl:-translate-x-2/4'>
+			{children}
+		</div>
+	);
+};
+
+export default TopMessage;
