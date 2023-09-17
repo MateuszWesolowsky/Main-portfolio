2b8abb2383a1f6c5a076f4717f91d9a34fb6915c
Mateusz <m.wesolowski.92@gmail.com>
2023-09-17 21:01:49 +0200

    create import/export ts file

---
+++ src/components/index.ts
@@ -0,0 +1,10 @@
+import Button from "./Button";
+import Card from "./Card";
+import Menu from "./Menu";
+import Reveal from "./Reveal";
+import SocialMediaIcon from "./SocialMediaIcon";
+import TopMessage from "./TopMessage";
+
+
+
+export { Button, Menu, SocialMediaIcon, Card, Reveal, TopMessage };
